014eca5efc7f5f13ad78bca10d0c0486fb30db46
Jonas Carpay <jonascarpay@gmail.com>
2020-12-01 18:35:02 +0900

    self nerd-snipe on the generalization to any tuple

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,39 @@
-module Lib
-  ( someFunc,
-  )
-where
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "hey"
+module Lib where
+
+import Control.Applicative
+import Control.Monad.Reader
+import Control.Monad.State
+
+{-- fluff
+uniquePairs :: [a] -> [(a, a)]
+uniquePairs [] = []
+uniquePairs (a : t) = (fmap (a,) t) <> uniquePairs t
+
+uniqueTriplets :: [a] -> [(a, a, a)]
+uniqueTriplets [] = []
+uniqueTriplets (a : t) = (tcons a <$> uniquePairs t) <> uniqueTriplets t
+ where
+  tcons a (b, c) = (a, b, c)
+--}
+
+uniques :: Traversable t => t () -> [a] -> [t a]
+uniques base as =
+  flip evalStateT as $
+    forM base $
+      const $
+        fix $ \f ->
+          get >>= \case
+            [] -> empty
+            (h : t) -> put t >> (pure h <|> f)
+
+uniqueTuples :: (Traversable t, Applicative t) => [a] -> [t a]
+uniqueTuples = uniques (pure ())
+
+readListOfInts :: FilePath -> IO [Int]
+readListOfInts = fmap parseInts . readFile
+ where
+  parseInts :: String -> [Int]
+  parseInts = fmap read . lines
