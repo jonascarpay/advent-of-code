e3abd4a01f6876ec984591c202138a41b2f5851f
Jonas Carpay <jonascarpay@gmail.com>
2020-12-22 02:24:15 +0900

    ormolu hlint

--- src/Runners.hs
+++ src/Runners.hs
@@ -47,38 +47,38 @@ pp = do
         chunk "(contains "
         is <- sepBy word (chunk ", ")
         chunk ")"
-        pure $ (ws, is)
+        pure (ws, is)
   many $ sub <* eol
 
 solve2 :: [([Text], [Text])] -> (Int, Text)
 solve2 cands = (occurs, dangers)
- where
-  occurs :: Int
-  occurs = sum $ (`H.lookup` nosh) <$> nos
-  dangers :: Text
-  dangers = T.intercalate "," $ fmap snd $ fromJust $ assignUnique (fmap S.toList <$> M.toList allergs)
-  nosh :: H.Histogram Text
-  nosh = H.fromList (cands >>= fst)
-  nos :: [Text]
-  nos = filter (\f -> S.notMember f hasAllerg) foods
-  allergs :: Map Text (Set Text)
-  allergs = go mempty cands
-  foods :: [Text]
-  foods = S.toList $ S.fromList $ cands >>= fst
-  hasAllerg :: Set Text
-  hasAllerg = fold $ M.elems $ allergs
-  go :: Map Text (Set Text) -> [([Text], [Text])] -> Map Text (Set Text)
-  go m [] = m
-  go m ((is, as) : t) =
-    let m' = foldr (\a n -> M.insertWith S.intersection a (S.fromList is) n) m as
-     in go m' t
+  where
+    occurs :: Int
+    occurs = sum $ (`H.lookup` nosh) <$> nos
+    dangers :: Text
+    dangers = T.intercalate "," $ fmap snd $ fromJust $ assignUnique (fmap S.toList <$> M.toList allergs)
+    nosh :: H.Histogram Text
+    nosh = H.fromList (cands >>= fst)
+    nos :: [Text]
+    nos = filter (`S.notMember` hasAllerg) foods
+    allergs :: Map Text (Set Text)
+    allergs = go mempty cands
+    foods :: [Text]
+    foods = S.toList $ S.fromList $ cands >>= fst
+    hasAllerg :: Set Text
+    hasAllerg = fold $ M.elems allergs
+    go :: Map Text (Set Text) -> [([Text], [Text])] -> Map Text (Set Text)
+    go m [] = m
+    go m ((is, as) : t) =
+      let m' = foldr (\a n -> M.insertWith S.intersection a (S.fromList is) n) m as
+       in go m' t
 
 day21 :: IO ()
 day21 = do
   p <- parseFile "input/day21.txt" pp
   let (s1, s2) = solve2 p
-  print $ s1
-  print $ s2
+  print s1
+  print s2
 
 {--
    -- Day 20 crap
