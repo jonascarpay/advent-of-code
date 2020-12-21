415644e1c73671d641a13d8f6041207681917a8f
Jonas Carpay <jonascarpay@gmail.com>
2020-12-22 02:24:25 +0900

    update to new nix template

--- shell.nix
+++ shell.nix
@@ -1,21 +1,20 @@
 let
-  hsPkgs = import ./.;
-  pkgs = import <nixpkgs> { };
+  pkgs = import ./pkgs.nix;
+  hsPkgs = pkgs.hsPkgs;
+  ormolu = pkgs.haskell-nix.tool hsPkgs.projectArgs.compiler-nix-name "ormolu" "0.1.4.1";
+  # Warning: only works with ghc 8.10 and up!
+  ormolu-wrapped = pkgs.writeShellScriptBin "ormolu" ''
+    ${ormolu}/bin/ormolu --ghc-opt=-XImportQualifiedPost $@
+  '';
 in
 hsPkgs.shellFor {
   withHoogle = true;
   tools = {
     cabal = "3.2.0.0";
-    hlint = "3.2.2";
+    hlint = "3.2.3";
     ghcid = "0.8.7";
-    ghcide = "0.2.0";
-    # ormolu = "0.1.4.1";
-    fourmolu = "0.3.0.0";
+    haskell-language-server = "0.6.0";
   };
-  buildInputs = [
-    (pkgs.writeShellScriptBin "ormolu" ''
-      fourmolu --indentation=2 --ghc-opt=-XImportQualifiedPost $@
-    '')
-  ];
+  buildInputs = [ ormolu-wrapped ];
   exactDeps = true;
 }
