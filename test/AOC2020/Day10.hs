9cefd90e126e33f4f73e9c036a9a8802574e4420
Jonas Carpay <jonascarpay@gmail.com>
2021-12-07 04:06:24 +0900

    2020 Day 10 fixup

--- test/AOC2020/Day10.hs
+++ test/AOC2020/Day10.hs
@@ -1,29 +1,45 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 module AOC2020.Day10 (day10) where
 
-import Data.List (foldl', sort)
+import Data.List (sort)
 import Data.Map (Map)
 import Data.Map qualified as M
-import Data.Maybe
+import Parse
 import Test.Hspec
 import TestLib
 
+type Input = [Int]
+
+parser :: Parser Input
+parser = pLines decimal
+
+ex1 :: Input -> Int
+ex1 = go 0 0 0 . sort
+  where
+    go n1 n3 j (j' : js)
+      | j' == j + 1 = go (n1 + 1) n3 j' js
+      | j' == j + 2 = go n1 n3 j' js
+      | j' == j + 3 = go n1 (n3 + 1) j' js
+    go n1 n3 _ _ = n1 * (n3 + 1)
+
+ex2 :: Input -> Int
+ex2 = go (M.singleton 0 1) . sort
+  where
+    ways :: Int -> Map Int Int -> Int
+    ways j m = let f i = M.findWithDefault 0 i m in f (j - 1) + f (j - 2) + f (j - 3)
+    go :: Map Int Int -> [Int] -> Int
+    go m [] = ways (maximum $ M.keys m) m
+    go m (h : t) = go (M.insert h (ways h m) m) t
+
 day10 :: Spec
 day10 = do
-  nums :: [Int] <- runIO $ fmap read . lines <$> readFile "input/2020/day10.txt"
-  let go :: Int -> Int -> Int -> [Int] -> Int
-      go n n1 n3 [] = (n1 * (n3 + 1))
-      go n n1 n3 (x : xs)
-        | x - n == 1 = go x (n1 + 1) n3 xs
-        | otherwise = go x n1 (n3 + 1) xs
-  star1 2048 $ go 0 0 0 nums
-  let goal = maximum nums + 3
-  let f :: Int -> Map Int Int -> Map Int Int
-      f n m =
-        let ways = sum $ (\n' -> fromMaybe 0 (M.lookup n' m)) <$> ([(n - 3) .. (n - 1)])
-         in M.insert n ways m
-      m = foldl' (flip f) (M.singleton 0 1) (sort (goal : nums))
-  star2 1322306994176 $ m M.! goal
+  exam <- runIO $ parseFile "input/2020/day10ex.txt" parser
+  input <- runIO $ parseFile "input/2020/day10.txt" parser
+  star1ex 220 $ ex1 exam
+  star1 2048 $ ex1 input
+  star2ex 19208 $ ex2 exam
+  star2 1322306994176 $ ex2 input
